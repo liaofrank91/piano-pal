dbf2f3366786c88899e0e8e6745efc5d160b6946
Frank Liao <liao.frank.91@gmail.com>
2022-07-28 23:19:42 -0400

    prep for heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
+const path = require('path')
 const express = require('express')
 // ^ common JS module syntax
 const colors = require('colors')
 const dotenv = require('dotenv').config()
-const {errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const PORT = process.env.PORT || 8000
 
@@ -15,14 +16,21 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: "Welcome to the Practice Assistant API"})
-})
-
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/songs', require('./routes/songRoutes'))
 app.use('/api/repertoire', require('./routes/repertoireRoutes'))
+
+// Serve Frontend 
+if (process.env.NODE_ENV === 'production') {
+    // set build folder as static
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.status(200).json({ message: "Welcome to the Practice Assistant API" })
+    })
+}
 
 app.use(errorHandler) // lets us use middleware
 
